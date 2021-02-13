68d6c6c3b45cc71084a273b6b481b0c0823648a1
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-02-13 21:21:55 +0530

    forign keyy test

--- albums/models.py
+++ albums/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    #image_url=models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
+class Photo(models.Model):
+    image_url= models.ForeignKey(Album,on_delete=models.CASCADE)
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    def __str__(self):
+        return self.image_url
