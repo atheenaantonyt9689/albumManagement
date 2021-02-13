ae7b431e72c0cd08f9c4eea4010efa7200b49910
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-02-14 05:18:30 +0530

    foreign keyyy errrorrr

--- albumManagemnt/urls.py
+++ albumManagemnt/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include, reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
-   #path("", RedirectView.as_view(url=reverse_lazy("album-list"))),
+    path("", RedirectView.as_view(url=reverse_lazy("album-list"))),
     path("admin/", admin.site.urls),
     path("", include("albums.urls")),
 ]
--- albums/models.py
+++ albums/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    #image_url=models.CharField(max_length=200)
-
+    
     def __str__(self):
         return self.title
 class Photo(models.Model):
-    image_url= models.ForeignKey(Album,on_delete=models.CASCADE)
+    image_url= models.CharField(max_length=200)
     description = models.TextField()
-    datetime = models.DateTimeField()
+    posted_at = models.DateTimeField()
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+
     def __str__(self):
         return self.image_url
